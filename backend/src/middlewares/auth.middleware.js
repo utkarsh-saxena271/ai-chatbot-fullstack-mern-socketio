4f3ddb6900cf1ebdba0bbd8b6373758a6df35cfe
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2025-09-20 15:30:14 +0530

    added auth middlewares for user authentication

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,32 @@
+const userModel = require('../models/user.model')
+const jwt = require('jsonwebtoken')
+
+
+
+
+async function authUser(req,res,next){
+    const {token} = req.cookie;
+
+    if(!token){
+        return res.status(401).json({
+            message: "unauthorized"
+        })
+    }
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await userModel.findById(decoded.id)
+        req.user = user
+        next()
+
+    }catch(err){
+        res.status(401).json({
+            message: "unauthorized"
+        })
+    }
+
+}
+
+
+module.exports = {
+    authUser
+};
