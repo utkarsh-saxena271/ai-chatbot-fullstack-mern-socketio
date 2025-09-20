8b6ae24294f6d00974af504b9611994a1ca5fa5d
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2025-09-20 15:47:23 +0530

    added chat routes and comments

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,19 @@
 const express = require('express')
-const app = express()
 const cookieParser = require('cookie-parser')
+
+const app = express()
+
+// Routes
 const authRoutes = require('./routes/auth.routes')
+const chatRoutes = require('./routes/chat.route')
 
-
+//Using Middlewares
 app.use(express.json())
 app.use(cookieParser())
+
+//Using Routes
 app.use('/api/auth',authRoutes)
+app.use('/api/chat',chatRoutes)
 
 
 
