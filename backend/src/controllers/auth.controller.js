2c6834ba7673d989b69e9eadee4affd98a37a097
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2025-09-21 10:16:30 +0530

    fixed 2 minor bugs related to registerController

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs')
 
 
 async function registerController(req,res){
-    const {fullname:{firstName, lastName}, email, password } = req.body;
+    const {fullName:{firstName, lastName}, email, password } = req.body;
     const isUserAlreadyExists = await userModel.findOne({
         email
     })
@@ -13,10 +13,10 @@ async function registerController(req,res){
             message:"user already exists, please use another email "
         })
     }
-    const hashPassword = bcrypt.hash(password,10)
+    const hashPassword = await bcrypt.hash(password,10)
 
     const user = await userModel.create({
-            fullname:{firstName, lastName}, 
+            fullName:{firstName, lastName}, 
             email, 
             password : hashPassword
         })
@@ -29,7 +29,7 @@ async function registerController(req,res){
         user:{
             email:user.email,
             id: user._id,
-            fullname: user.fullName
+            fullName: user.fullName
         }
     })
 }
@@ -52,13 +52,12 @@ async function loginController(req,res){
     }
     const token = jwt.sign({id:user._id},process.env.JWT_SECRET);
     res.cookie('token',token)
-
     res.status(201).json({
         message:"user logged in successfully",
         user:{
             email:user.email,
             id: user._id,
-            fullname: user.fullName
+            fullName: user.fullName
         }
     })
 }
