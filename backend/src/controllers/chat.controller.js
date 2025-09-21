85759052dfd840f648c830371c559125b38c17e9
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2025-09-21 11:33:48 +0530

    fixed a typo bug in chat controller

--- backend/src/controllers/chat.controller.js
+++ backend/src/controllers/chat.controller.js
@@ -2,11 +2,11 @@ const chatModel = require('../models/chat.model')
 
 
 async function createChat(req,res){
-    const {title} = req.body();
+    const {title} = req.body;
     const user = req.user;
 
     const chat = await chatModel.create({
-        user: user_.id,
+        user: user._id,
         title
     })
 
