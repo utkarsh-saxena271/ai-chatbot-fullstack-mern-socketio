c6ceb89acdc1e3319e902016ff4ab9ba986ea936
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2025-09-21 17:01:28 +0530

    integrated gemini ai service for ai responses

---
+++ backend/src/services/ai.service.js
@@ -0,0 +1,13 @@
+const { GoogleGenAI } = require("@google/genai");
+
+const ai = new GoogleGenAI({});
+
+async function generateResponse(content){
+    const response = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: content ,
+    })
+    return response.text
+}
+
+module.exports = {generateResponse};
