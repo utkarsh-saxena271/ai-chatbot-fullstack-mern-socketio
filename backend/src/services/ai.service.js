a869d9f71a1e00ee9b6b3d4aeb086904b7e72494
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2025-09-22 20:51:50 +0530

    added generateVector in ai service, and created vector service using pinecone as vector db, added creatememory and querymemory in vector service

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -10,4 +10,21 @@ async function generateResponse(content){
     return response.text
 }
 
-module.exports = {generateResponse};
+async function generateVector(content){
+    const response = await ai.models.embedContent({
+        model: 'gemini-embedding-001',
+        contents: content,
+        config:{
+            outputDimensionality:768
+        }
+    });
+
+    return response.embeddings[0].values 
+}
+
+
+
+module.exports = {
+    generateResponse,
+    generateVector
+};
---
+++ backend/src/services/vectors.service.js
@@ -0,0 +1,30 @@
+// Import the Pinecone library
+const { Pinecone } = require('@pinecone-database/pinecone')
+
+// Initialize a Pinecone client with your API key
+const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY});
+
+// Create a dense index with integrated embedding
+const chatGptIndex = pc.index('chatgpt-project')
+
+async function createMemory({vectors,metadata,messageId}){
+    await chatGptIndex.upsert([{
+        id : messageId,
+        values: vectors,
+        metadata
+    }])
+}
+async function queryMemory({queryVector, limit = 5, metadata}){
+
+    const data = await chatGptIndex.query({
+        vector:queryVector,
+        topK:limit,
+        filter:metadata?{metadata}:undefined 
+    })
+    return data.matches;
+}
+
+module.exports = {
+    createMemory,
+    queryMemory
+}
